f0f5d8f1572ed1c6c3feca826f8ea018aa7bbb6a
Bryan <bryan@Bryans-MacBook-Air.local>
2022-08-30 22:09:58 +0800

    Create basic difficulty selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
+import DifficultyPage from "./components/DifficultyPage";
 import {Box} from "@mui/material";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/difficulty" element={<DifficultyPage/>}/>
                     </Routes>
                 </Router>
             </Box>
