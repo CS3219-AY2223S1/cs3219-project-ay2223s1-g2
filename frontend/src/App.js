1fecfcb648b9207f327709eef6b84e803b2ab161
johnson yee <johnsenyee@gmail.com>
2022-09-13 02:53:43 +0800

    FR1.3: Front end changes for sign in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
+import LoginPage from "./components/LoginPage";
 import {Box} from "@mui/material";
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
             <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
                 <Router>
                     <Routes>
-                        <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
+                        <Route exact path="/" element={<Navigate replace to="/login" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/login" element={<LoginPage/>}/>
                     </Routes>
                 </Router>
             </Box>
--- user-service/index.js
+++ user-service/index.js
@@ -7,12 +7,14 @@ app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
 import { createUser } from './controller/user-controller.js';
+import { logUserIn } from './controller/auth-controller.js';
 
 const router = express.Router()
 
 // Controller will contain all the User-defined Routes
 router.get('/', (_, res) => res.send('Hello World from user-service'))
 router.post('/', createUser)
+router.post("/login", logUserIn);
 
 app.use('/api/user', router).all((_, res) => {
     res.setHeader('content-type', 'application/json')
