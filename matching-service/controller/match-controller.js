f4fd4de2c3806e935d0c85bf31aad87a6bb83e01
howardwhw2 <howardwhw2@gmail.com>
2022-09-12 20:13:40 +0800

    Remove unused line

--- matching-service/controller/match-controller.js
+++ matching-service/controller/match-controller.js
@@ -3,7 +3,7 @@ import { ormMatchUser } from '../model/match-user.js'
 
 export const matchController = function (socket) {
     console.log("connected");
-    socket.join("room");
+
     socket.on("matchInit", async (data, callback) => {
         if (await ormMatchUser(socket, data.difficulty)) {
             ormInitiateMatch(socket.id, data);
