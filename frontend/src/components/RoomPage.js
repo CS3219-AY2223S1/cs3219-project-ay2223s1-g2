40f6a30045f03bf587fba38a4bc22401e69f248c
blimyj <blimyj21@gmail.com>
2022-09-12 18:53:35 +0800

    Add functionality to leave room

--- frontend/src/components/RoomPage.js
+++ frontend/src/components/RoomPage.js
@@ -13,14 +13,19 @@ import {useState} from "react";
 import axios from "axios";
 import {URL_USER_SVC} from "../configs";
 import {STATUS_CODE_CONFLICT, STATUS_CODE_CREATED} from "../constants";
-import {Link, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 
 function RoomPage() {
     const params = useParams();
-    
+    const navigate = useNavigate();
+    const handleNavigateHome = () => {
+        navigate('/homepage')
+    }
+
     return (
         <Box display={"flex"} flexDirection={"column"} width={"100%"}>
             <Typography variant={"h3"} marginBottom={"2rem"}>Room Number {params.id}</Typography>
+            <Button size={"Medium"} variant={"outlined"} onClick={handleNavigateHome}>Home</Button>
         </Box>
     )
 }
