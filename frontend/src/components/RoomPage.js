476843213e151adf212b0b0bd2a915f34d8bd4ac
blimyj <blimyj21@gmail.com>
2022-10-09 03:14:54 +0800

    Add functionality to display question data on frontend

--- frontend/src/components/RoomPage.js
+++ frontend/src/components/RoomPage.js
@@ -13,18 +13,24 @@ import {useState} from "react";
 import axios from "axios";
 import {URL_USER_SVC} from "../configs";
 import {STATUS_CODE_CONFLICT, STATUS_CODE_CREATED} from "../constants";
-import {Link, useNavigate, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams, useLocation} from "react-router-dom";
 
 function RoomPage() {
     const params = useParams();
     const navigate = useNavigate();
+    const {state} = useLocation();
+    const { question } = state;
+    console.log(question.data[0])
+    const questionData = question.data[0]
     const handleNavigateHome = () => {
         navigate('/homepage')
     }
 
     return (
         <Box display={"flex"} flexDirection={"column"} width={"100%"}>
-            <Typography variant={"h3"} marginBottom={"2rem"}>Room Number {params.id}</Typography>
+            <Typography variant={"h7"} marginBottom={"2rem"}>Room Number {params.id}</Typography>
+            <Typography variant={"h3"} marginBottom={"2rem"}>Title: {questionData.title}</Typography>
+            <Typography variant={"h5"} marginBottom={"2rem"}>{questionData.questionDesc}</Typography>
             <Button size={"Medium"} variant={"outlined"} onClick={handleNavigateHome}>Home</Button>
         </Box>
     )
