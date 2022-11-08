6bf26b3df63fd24ce9003c0fe01c1e085e646372
blimyj <blimyj21@gmail.com>
2022-11-08 23:27:07 +0800

    Minor fix to Github Workflow

--- question-service/question-service-mongodb/seed-mongodb/seed-mongodb.js
+++ question-service/question-service-mongodb/seed-mongodb/seed-mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import { Question } from './question-model.js'
 import { questionData } from './questionData.js'
 
-mongoose.connect('mongodb://localhost:5100', {
+mongoose.connect('mongodb://localhost:5100/test', {
 	useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => {
 	console.log('Connected to MongoDB instance.')
